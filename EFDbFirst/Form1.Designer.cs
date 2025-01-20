9362feb33aa707dffe2ceec00ef87cabcfd4fec5
Pashaie <pashaie@gmail.com>
2025-01-20 18:41:47 +0330

    db first

--- EFDbFirst/Form1.Designer.cs
+++ EFDbFirst/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonAddUser = new System.Windows.Forms.Button();
+            this.dataGridViewUsers = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonAddUser
+            // 
+            this.buttonAddUser.Location = new System.Drawing.Point(12, 12);
+            this.buttonAddUser.Name = "buttonAddUser";
+            this.buttonAddUser.Size = new System.Drawing.Size(136, 39);
+            this.buttonAddUser.TabIndex = 0;
+            this.buttonAddUser.Text = "Add User";
+            this.buttonAddUser.UseVisualStyleBackColor = true;
+            this.buttonAddUser.Click += new System.EventHandler(this.buttonAddUser_Click);
+            // 
+            // dataGridViewUsers
+            // 
+            this.dataGridViewUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewUsers.Location = new System.Drawing.Point(12, 57);
+            this.dataGridViewUsers.Name = "dataGridViewUsers";
+            this.dataGridViewUsers.Size = new System.Drawing.Size(776, 381);
+            this.dataGridViewUsers.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridViewUsers);
+            this.Controls.Add(this.buttonAddUser);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsers)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonAddUser;
+        private System.Windows.Forms.DataGridView dataGridViewUsers;
     }
 }
 
--- EFDbFirst/Form1.cs
+++ EFDbFirst/Form1.cs
@@ -16,5 +16,39 @@ namespace EFDbFirst
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //create db context
+            // db.User().Where.ToList()
+
+            using (var db = new cs140310Entities())
+            {
+                /*
+                 * MyFunc(User u) {return u.IsActive == true;}
+                 * (User x) => {return  x.IsActive == true;}
+                 * (x) => {return  x.IsActive == true;}
+                 * x => x.IsActive == true
+                 */
+                //Lazy 
+                //E
+                dataGridViewUsers.DataSource = db.Users
+                    .Where(u => u.IsActive == true)
+                    .OrderBy(u => u.FirstName)
+                    .ToList();
+            }
+        }
+
+        private void buttonAddUser_Click(object sender, EventArgs e)
+        {
+            using (var db = new cs140310Entities())
+            {
+                var user = new User { IsActive = true, FirstName = "Samira", Family = "Mohsenzadeh", Email = "N/A" };
+                db.Users.Add(user);
+                //db.Users.Add(user2);
+                //db.Products.Add(new Product { Name = "PC", Price = 5000, IsActive = true});
+                db.SaveChanges();
+            }
+        }
     }
 }
